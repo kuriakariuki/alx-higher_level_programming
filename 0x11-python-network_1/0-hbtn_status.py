dec5901f1ee0ac6be62e15ff8163dd560dbf9313
KuriaKariuki <kariukijohnk1@gmail.com>
2022-09-25 17:10:28 +0300

    Added 5 files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" Get https://intranet.hbtn.io/status"""
+"""Fetches https://intranet.hbtn.io/status"""
+
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')as response:
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(html)}")
-        print(f"\t- content: {html}")
-        print("\t- utf8 content: {html.decode("utf-8", "replace")}")
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
